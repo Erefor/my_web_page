b901a0e55a731e3db9cfa1157b56e81be67185dc
José <perezg.josesal@gmail.com>
2021-05-19 22:49:44 -0500

    Icons row added with some more features

--- lib/Pages/AboutMePage.dart
+++ lib/Pages/AboutMePage.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -10,13 +11,15 @@ class AboutMePage extends StatelessWidget {
       backgroundColor: Color(0xff153D75),
       body: SingleChildScrollView(
         child: Center(
-          heightFactor: 2,
           child: Wrap(
             alignment: WrapAlignment.center,
-            spacing: 10,
+            spacing: 20,
+            direction: Axis.horizontal,
             children: [
+              //_IconsSection(),
               _DescriptionWidget(),
               _ContactInfoWidget(),
+              _IconsSection(),
             ],
           ),
         ),
@@ -36,27 +39,69 @@ class _DescriptionWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(25),
       ),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            kDescription,
-            textAlign: TextAlign.justify,
-            style: GoogleFonts.roboto(
-              color: Colors.white,
-              fontSize: 25,
-              fontWeight: FontWeight.w300,
+          FadeInLeftBig(
+            child: Text(
+              kDescription,
+              textAlign: TextAlign.center,
+              style: GoogleFonts.roboto(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
             ),
           ),
-          SizedBox(height: 115),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _CustomDescriptionIcon(icon: FontAwesomeIcons.html5),
-              _CustomDescriptionIcon(icon: FontAwesomeIcons.css3),
-              _CustomDescriptionIcon(icon: FontAwesomeIcons.js),
-              _CustomDescriptionIcon(icon: FontAwesomeIcons.vuejs),
-              _CustomDescriptionIcon(icon: FontAwesomeIcons.unity),
-            ],
-          )
+        ],
+      ),
+    );
+  }
+}
+
+class _IconsSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 25),
+      child: Wrap(
+        spacing: 30,
+        children: <Widget>[
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.html5,
+            seconds: 0,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.css3,
+            seconds: 1,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.js,
+            seconds: 2,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.vuejs,
+            seconds: 3,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.android,
+            seconds: 4,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.react,
+            seconds: 5,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.gitAlt,
+            seconds: 6,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.github,
+            seconds: 7,
+          ),
+          _CustomDescriptionIcon(
+            icon: FontAwesomeIcons.bootstrap,
+            seconds: 8,
+          ),
         ],
       ),
     );
@@ -65,11 +110,15 @@ class _DescriptionWidget extends StatelessWidget {
 
 class _CustomDescriptionIcon extends StatelessWidget {
   final IconData icon;
+  final int seconds;
 
-  const _CustomDescriptionIcon({this.icon = Icons.ac_unit_rounded});
+  const _CustomDescriptionIcon(
+      {this.icon = Icons.ac_unit_rounded, this.seconds = 0});
   @override
   Widget build(BuildContext context) {
-    return Icon(icon, color: Colors.white, size: 40);
+    return FadeIn(
+        delay: Duration(seconds: this.seconds),
+        child: Icon(icon, color: Colors.white, size: 50));
   }
 }
 
@@ -80,15 +129,24 @@ class _ContactInfoWidget extends StatelessWidget {
       width: 350,
       height: 450,
       decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(25),
-      ),
+          color: Colors.transparent,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              blurRadius: 10,
+              color: Colors.black,
+              offset: Offset(0, 5),
+            )
+          ]),
       child: Column(
         children: <Widget>[
           Expanded(
-            child: Image(
-              image: AssetImage('lib/assets/CEOS.jpg'),
-              fit: BoxFit.contain,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(25),
+              child: Image(
+                image: AssetImage('lib/assets/CEOS.jpg'),
+                fit: BoxFit.contain,
+              ),
             ),
           )
         ],
--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,2 +1,2 @@
 const String kDescription =
-    'My name is José Salvador, im a Software Engineering student and a Flutter developer. I have been working with Flutter for a long time. So far, i can implement SQlite, Provider, Firebase, QRscan, BloC and more. Also, i can implement any layout capable of displaying on any device.';
+    'My name is José Salvador, im a Software Engineering student and a Flutter developer. I have been working with Flutter for a long time. So far, i can implement SQlite, Provider, Firebase, QRscan, BloC and more. Also, i can implement any layout capable of displaying on any device. I have also developed web pages for some clients using basic technologies such as HTML5, CSS3, JS and other frameworks.';
